bdeb2b4b7ce15979435f8eb1dddb3d1f44b2f50a
Clément Denos <azekool123@gmail.com>
2020-01-18 17:51:38 +0100

    Modularité ameliorée et user info command

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const discord = require('discord.js');
 const bot = new discord.Client();
 const token = 'NjQ2MDI1NTcyMTE5NDc4Mjcy.XdLmxA.gVN9qtLnbL5gHok3Zzt3UEw_Gj4';
 const prefix ='>';
-const EmbedMessage = new discord.RichEmbed();
+const content = require('./content');
+const EmbedMessage = require('./embedMessages');
 
-const media = ['./media/bad.mp4', './media/biscotte.gif', './media/bonjour.gif', './media/BoNjOuR2.gif', './media/catastrophe.gif', './media/CHEH.mp4', './media/huile.gif', './media/mambo.gif', './media/sel.mp4'];
-const color = ['DEFAULT','WHITE','AQUA','GREEN','BLUE','PURPLE','LUMINOUS_VIVID_PINK','GOLD','ORANGE','RED','GREY','DARKER_GREY','NAVY','DARK_AQUA','DARK_GREEN','DARK_BLUE','DARK_PURPLE','DARK_VIVID_PINK','DARK_GOLD','DARK_ORANGE','DARK_RED','DARK_GREY','LIGHT_GREY','DARK_NAVY','RANDOM',
-  ]
 var dm = 'Bonjour, les différentes commandes disponibles sont : \n**oss + ** bonjour, bagarre, fight, hello, help, danse, oops, massage -> renvoie un gif ou une vidéo en lien avec OSS\n**sel** -> MAIS C\'ETAIT SUR EN FAIT!!\n**beer** -> You know what it is :kappa:\n**cheh** -> Très utile\n**hello** -> vous ne voulez pas essayer\n**random** -> renvoie un media random';
 var dmAdmin ='\nCertaines commandes nécessitent d\'être admin pour être éxécutés, parmi lesquels :\n**clear** + le nombre de messages à supprimer +1 -> supprime le nombre de messages donnés en arguments';
 
@@ -14,36 +12,6 @@ function LoggedIn(){
     bot.user.setStatus("online");
     bot.user.setActivity('>help', {type : 'PLAYING'});
     console.log('Bot Online');
-}
-
-function Random(length){
-    return Math.floor(Math.random()*Math.floor(length));
-}
-
-function content(args, msg){
-    msg.channel.bulkDelete(1);
-    switch(args[1]){
-        case 'sel':
-            msg.channel.send({files : [media[8]]});
-        break;
-        case 'beer':
-            
-            msg.channel.send('https://media.discordapp.net/attachments/603256095217156096/638069133707182112/hhBeer.gif');
-        break;
-        case 'cheh':
-            
-            msg.channel.send({files : [media[5]]});
-        break;
-        case 'hello':
-            
-            msg.reply('vous dit : ');
-            msg.channel.send({files : [media[3]]});
-        break;
-        case 'random': 
-            
-            msg.channel.send({files : [media[Random(media.length)]]});
-        break;
-    }
 }
 
 function oss(args, msg){
@@ -80,12 +48,6 @@ function oss(args, msg){
         msg.channel.send('Invalid Args');
 }
 
-function embed(guildMember, member){
-    EmbedMessage.setColor(color[Random(color.length)]);
-    EmbedMessage.setTitle('La profile pic de '+guildMember.nickname+' : ');
-    EmbedMessage.setImage(member.avatarURL);
-}
-
 function user(args, msg){
     var guildMember = msg.guild.member(msg.mentions.users.first());
     var mentionedUser = msg.mentions.users.first();
@@ -96,8 +58,7 @@ function user(args, msg){
             msg.member.user.send(dmAdmin);
         break;
         case 'info':
-            embed(guildMember, mentionedUser);
-            msg.channel.send(EmbedMessage);
+            msg.channel.send(EmbedMessage.userInfo(guildMember, mentionedUser));
         break;
     }
 }
@@ -113,13 +74,12 @@ bot.on('message', msg=>{
                 oss(args, msg);
             break;
             case 'content':
-                content(args, msg);
+                content.reaction(args, msg);
             break;
             case 'user':
                 user(args, msg);
             break;
             case 'clear':
-                msg.channel.bulkDelete(1);
                 if (msg.member.hasPermission('ADMINISTRATOR')){
                     if (!args[1])
                         msg.reply('Merci de saisir des arguments valides ! >:(');
