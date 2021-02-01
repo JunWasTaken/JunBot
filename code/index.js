ecb152a0eea6b5563b519e1cfd361468250e885c
Jun <47633500+JunkieUnicorn@users.noreply.github.com>
2021-02-01 18:50:37 +0100

    ajout de gitignore

--- code/index.js
+++ code/index.js
@@ -1,4 +1,5 @@
 const discord = require('discord.js');
+const musicClient = require('yet-another-discord.js-musicbot-addon');
 const bot = new discord.Client();
 const token = 'NjQ2MDI1NTcyMTE5NDc4Mjcy.XdLmxA.gVN9qtLnbL5gHok3Zzt3UEw_Gj4';
 const prefix ='>';
@@ -6,12 +7,23 @@ const userFunction = require('./user');
 const content = require('./content');
 const musicFunction = require('./music');
 const useful = require('./useful');
+const MusicClient = require('yet-another-discord.js-musicbot-addon');
 
 function LoggedIn(){
     bot.user.setStatus("online");
     bot.user.setActivity('>help', {type : 'PLAYING'});
     console.log('Bot Online');
 }
+
+bot.music = new MusicClient(bot, {
+    apiKey : "AIzaSyCl4n-fnM_P6nDZh7WLbagv917fSjWUIVc",
+    defVolume : 50,
+    bitRate : 12000,
+    maxHistory : 25,
+    maxQueue: 100, 
+    searchFilters: ['cover', 'live', 'remix', 'mix', 'parody', 'hour', 'extended', 'trailer'],
+    color: 13632027,
+});
 
 bot.on('ready', LoggedIn);
 
@@ -38,24 +50,29 @@ bot.on('message', msg=>{
             case 'help':
                 content.help(msg);
             break;
-            /*case 'clear':
-                console.log(msg.author.);
-                if (msg.guild.member(msg.author).hasPermission('ADMINISTRATOR') || msg.author.id == 271733914291142656){
+            case 'clear':
+                console.log(msg.guild.owner);
+                console.log(msg.guild.members);
+                //console.log(guild.member(msg.author));
+                /*if (msg.guild.member(msg.author).hasPermission('ADMINISTRATOR') || msg.author.id == 271733914291142656){
                     if (!args[1])
                         msg.reply('Merci de saisir des arguments valides ! >:(');
                     else{
                         msg.channel.bulkDelete(args[1]);
                     }
                 }else
-                    msg.reply('Tu n\'as pas les droits petit coquinou ;)');
+                    msg.reply('Tu n\'as pas les droits petit coquinou ;)');*/
             break;
             case 'music':
-                musicFunction.music(msg, args);
-            break;*/
+                //console.log(bot.music);
+                bot.music.joinFunction(msg);
+                bot.music.playFunction(msg, args[1]);
+                //musicFunction.music(msg, args, bot.music);
+            break;
         }
-    }else if (msg.content == "yes" || msg.content == ':yes:'){
+    }else if (msg.content == "yes" || msg.content == ":yes:"){
         msg.channel.send(useful.emoji(bot, "795738291819249694"));
-    }else if ((msg.content.includes("owo") || msg.content.includes("OwO") || msg.content.includes("OwO") || msg.content.includes("uwu") || msg.content.includes("UwU") || msg.content.includes("UWU")) && msg.author.id != 646025572119478272){
+    }else if ((msg.content.toUpperCase().includes("OWO") || msg.content.toUpperCase().includes("UWU")) && msg.author.id != 646025572119478272){
         msg.channel.send(useful.furry());
     }
 })
--- code/music.js
+++ code/music.js
@@ -4,20 +4,20 @@ var connection;
 var servers = {};
 
 module.exports = {
-    music:function(msg, args){
-        play(args, msg);
+    music:function(msg, args, client){
+        play(args, msg, client);
     }
 }
 
-function logToChannel(msg){
-    if (msg.member.voiceChannel){
-        connection = msg.member.voiceChannel.join();
+function logToChannel(msg, client){
+    if (msg.member){
+        client.joinFunction(msg);
     }else{
         msg.reply('vous n\'êtes pas dans un channel vocal');
     }
 }
 
-function play(args, msg){
+function play(args, msg, client){
     if (!args[1]){
         return msg.reply('vous devez mettre un lien youtube pour que ça fonctionne ._.').catch(console.error);
     }else{
@@ -26,10 +26,13 @@ function play(args, msg){
         }
         var server = servers[msg.guild.id]
         server.queue.push(args[1]);
+        console.log(server.queue);
         if (!msg.guild.voiceConnection){
             logToChannel(msg);
         }
-        server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: 'audioonly'}));
+
+        client.playFunction(msg, args[1]);
+        /*server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: 'audioonly'}));
         server.queue.shift();
         server.dispatcher.on("end", function(){
             if (server.queue[0]){
@@ -37,7 +40,7 @@ function play(args, msg){
             }else{
                 connection.disconnect();
             }
-        })
+        })*/
     }
 }
 
